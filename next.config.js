bcadf7a53333b269dd3fdc16d5f33f66226903e4
LuisJimenez19 <luisjian948@gmail.com>
2023-07-26 23:54:07 -0300

    7

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/about",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
